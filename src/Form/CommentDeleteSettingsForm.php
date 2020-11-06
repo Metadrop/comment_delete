8fb3f494339171c033b5b8f6bcc43ecdb1bdc58e
Olalla Rodicio <olalla.rodicio@metadrop.net>
2020-11-06 10:33:28 +0100

    Drupal 9 compatibility.

--- src/Form/CommentDeleteSettingsForm.php
+++ src/Form/CommentDeleteSettingsForm.php
@@ -31,34 +31,34 @@ class CommentDeleteSettingsForm extends ConfigFormBase {
     $config = $this->config('comment_delete.config');
     $form['comment_delete_default'] = [
       '#type' => 'radios',
-      '#title' => t('Default Delete Option'),
-      '#description' => t('Choose the default option selected when a comments deleted.'),
+      '#title' => $this->t('Default Delete Option'),
+      '#description' => $this->t('Choose the default option selected when a comments deleted.'),
       '#options' => [
-        0 => t('Delete comment and replies'),
-        1 => t('Delete comment and move replies up'),
-        2 => t('Delete comment and keep replies'),
+        0 => $this->t('Delete comment and replies'),
+        1 => $this->t('Delete comment and move replies up'),
+        2 => $this->t('Delete comment and keep replies'),
       ],
       '#required' => TRUE,
       '#default_value' => $config->get('default_selection'),
     ];
     $form['comment_delete_soft'] = [
       '#type' => 'checkbox',
-      '#title' => t('Soft delete comments'),
-      '#description' => t('When enabled the comment subject+body is cleared but comment+author is retained.'),
+      '#title' => $this->t('Soft delete comments'),
+      '#description' => $this->t('When enabled the comment subject+body is cleared but comment+author is retained.'),
       '#default_value' => $config->get('soft'),
     ];
     $form['comment_delete_threshold'] = [
       '#type' => 'textfield',
-      '#title' => t('Threshold Period'),
-      '#description' => t('Max allowable time comments can be deleted after creation. Enter zero (0) to disable.'),
+      '#title' => $this->t('Threshold Period'),
+      '#description' => $this->t('Max allowable time comments can be deleted after creation. Enter zero (0) to disable.'),
       '#size' => 10,
       '#default_value' => $config->get('threshold'),
       '#required' => TRUE,
     ];
     $form['comment_delete_message'] = [
       '#type' => 'textarea',
-      '#title' => t('Confirmation Message'),
-      '#description' => t('Customize confirmation message shown after comment has been deleted.'),
+      '#title' => $this->t('Confirmation Message'),
+      '#description' => $this->t('Customize confirmation message shown after comment has been deleted.'),
       '#default_value' => $config->get('message'),
       '#required' => TRUE,
     ];
